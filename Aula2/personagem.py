9f2048047e992fa4fc8acd689898bca529ccec2e
Laine Rodrigues Barros <laine.barros@estudante.ifgoiano.edu.br>
2026-08-18 15:41:48 -0300

    personagem.py
    Código em pytton que implementam a classe Personagem, aplicando os conceitos de classes, atributos e métodos que foram aprendidos em aula

---
+++ Aula2/personagem.py
@@ -0,0 +1,36 @@
+class Personagem:
+    def __init__(self, nome, vida, forca):
+      self.nome = nome
+      self.vida = vida
+      self.forca = forca
+def receberDano(self, dano):
+self.vida = self.vida - dano
+
+def estaVivo(self):
+return self.vida > 0
+
+def ficha(self):
+    print("Nome:", self.nome)
+    print("Vida:", self.vida)
+    print("Forca:", self.forca)
+
+def atacar(self, alvo):
+  alvo.receberDano(self.forca)
+
+if __name__ == "__main__":
+    heroi = Personagem("Mari", 100, 20)
+    chefe = Personagem("Laine", 100, 10)
+
+  print("ANTES DA BATALHA")
+    heroi.ficha()
+    chefe.ficha()
+
+ while heroi.estaVivo() and chefe.estaVivo():
+    heroi.atacar(chefe)
+
+if chefe.estaVivo():
+  chefe.atacar(heroi)
+
+print("DEPOIS DA BATALHA")
+  heroi.ficha()
+  chefe.ficha()
